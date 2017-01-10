9ef875c01f3a9ad9fa7213b8b76a555be18aec7c
Maximilian Litteral <maximilian@intrepid.io>
2017-01-10 15:46:33 -0500

    Add MarqueeLabel

--- CocoaGrapes/ViewController.swift
+++ CocoaGrapes/ViewController.swift
@@ -7,13 +7,55 @@
 //
 
 import UIKit
+import SwiftGifOrigin
+import MarqueeLabel
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    var label: MarqueeLabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dankImage = UIImage.gif(name: "dankgrapes")
+        imageView.image = dankImage
+        
+        label = MarqueeLabel(frame: CGRect.zero, rate: 30, fadeLength: 10.0)
+        label.text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+        label.animationCurve = .easeInOut
+        label.trailingBuffer = 10
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = true
+        label.holdScrolling = true
+        self.view.addSubview(label)
+        
+        // Tap gesture
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(labelWasTapped))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        label.addGestureRecognizer(tapGesture)
+        
+        // Constraints
+        var constraints: [NSLayoutConstraint] = []
+        constraints.append(label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor))
+        constraints.append(label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor))
+        constraints.append(label.bottomAnchor.constraint(equalTo: self.view.bottomAnchor))
+        self.view.addConstraints(constraints)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        label.pauseLabel()
+    }
+    
+    func labelWasTapped() {
+        label.holdScrolling = false
+//        label.unpauseLabel()
     }
 }
 
